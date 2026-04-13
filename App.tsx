b8042ba75cb0b6be686d2e1e31e7722826c547e8
cassiobernardibg009 <cassiobernardi.bg009@academico.ifsul.edu.br>
2026-04-13 12:31:36 -0300

    second commit

--- App.tsx
+++ App.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Register from './screens/Register';
-import Home from './screens/Home';
-
+import Register from './Screens/Register';
+import Home from './Screens/Home';
 export type RootStackParamList = {
   Register: undefined;
   Home: undefined;
--- firebase.ts
+++ firebase.ts
@@ -1,32 +1,19 @@
 import { initializeApp, getApps } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { initializeAuth, getAuth } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyAoKVNWa5_0Cbwe1ArKdrDQcPEQhzuErGI",
-
   authDomain: "biru-a1678.firebaseapp.com",
-
   projectId: "biru-a1678",
-
   storageBucket: "biru-a1678.firebasestorage.app",
-
   messagingSenderId: "708925927169",
-
   appId: "1:708925927169:web:6d33aec553eaa8ba2af500"
-
 };
 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-const app = getApps().length === 0
-  ? initializeApp(firebaseConfig)
-  : getApps()[0];
-
-// 🔥 AQUI ESTÁ A CORREÇÃO REAL
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-});
-
+export { auth };
 export const db = getFirestore(app);
